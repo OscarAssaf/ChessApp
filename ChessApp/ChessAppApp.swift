a8676ebf705e747a7fe3280e8abd4c8593339693
Zuets <asos.oscar@hotmail.com>
2025-02-17 15:36:21 +0100

    Initial Commit

---
+++ ChessApp/ChessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessAppApp.swift
+//  ChessApp
+//
+//  Created by Oscar on 2025-02-17.
+//
+
+import SwiftUI
+
+@main
+struct ChessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChessAppTests/ChessAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChessAppTests.swift
+//  ChessAppTests
+//
+//  Created by Oscar on 2025-02-17.
+//
+
+import Testing
+@testable import ChessApp
+
+struct ChessAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
